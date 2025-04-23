0be0380ef382e4c861b3fab72d7a26bda337195f
Muhammad Usman <mu163953@gmail.com>
2025-04-23 14:43:18 +0500

    add: show hide button in password field

--- src/components/templetes/form/PasswordField.tsx
+++ src/components/templetes/form/PasswordField.tsx
@@ -1,5 +1,7 @@
 import { PassworInputProps } from '@/types/typs';
-import React from 'react'
+import { FiEye } from "react-icons/fi";
+import { GoEyeClosed } from "react-icons/go";
+import React, { useState } from 'react'
 
 const PasswordField: React.FC<PassworInputProps> = ({
     className,
@@ -12,22 +14,30 @@ const PasswordField: React.FC<PassworInputProps> = ({
     id,
     padding,
     placeholder,
-    touched
+    touched,
+
 }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClick = () => {
+        setShowPassword((prev) => !prev);
+    };
     return (
-        <div className={`w-full ${padding} ${className}`} >
+        <div className={`w-full relative ${padding} ${className}`} >
             <label htmlFor={id}>
                 {label && <span className='text-sm color-primary font-ibmplexserif-regular p-2'>{label}</span>}
             </label>
             <input
                 id={id}
                 name={name}
-                type={type}
+                type={showPassword ? "text" : "password"}
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
                 className='w-full border border-gray-300 rounded-md p-2'
             />
+            <button type="button" onClick={handleClick} className='absolute right-2 top-7 w-8 h-8 flex items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300 transition duration-200'>
+                {showPassword ? <FiEye /> : <GoEyeClosed />}
+            </button>
             {error && <span className='text-red-500 text-sm'>{error}</span>}
         </div>
     )
